c2aa3424b74fd8f33a710ab0ea1ec9bb977ef346
adamsaleh11 <sadam1589@gmail.com>
2025-03-27 03:42:40 -0400

    Update font to space grotesk

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
   darkMode: ["class", "html"],
@@ -19,6 +19,9 @@ const config = {
       },
     },
     extend: {
+      fontFamily: {
+        sans: ["var(--font-space-grotesk)", "sans-serif"],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
@@ -76,16 +79,14 @@ const config = {
     },
   },
   plugins: [
-    // Only use the animate plugin if it's installed
     (() => {
       try {
-        return require("tailwindcss-animate")
+        return require("tailwindcss-animate");
       } catch (e) {
-        return null
+        return null;
       }
     })(),
   ].filter(Boolean),
-} satisfies Config
+} satisfies Config;
 
-export default config
-
+export default config;
